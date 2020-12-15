2d32e5e73c1beda36b4f0c8d06dcfac140459c01
patrik-setterberg <setterberg.patrik@gmail.com>
2020-12-15 22:11:59 +0100

    Add text input component

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import g from '../../globals.js';
 import words from '../../assets/game/words.js';
 
-const GameWrapper = styled.div`
+const TypeTestWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
   min-height: 500px;
   border: 2px solid #222;
   margin: 0 auto;
+  font-family: 'Roboto Mono', monospace;
   color: #fff;
   font-size: 1.5rem;
 
@@ -24,18 +26,61 @@ const GameWrapper = styled.div`
 
 `
 
-export default class TypeGame extends Component {
+const Text = styled.div`
 
-  constructor() {
-    super();
-    this.state = {}
-  }
+`
 
-  render() {
-    return(
-      <GameWrapper>
-        eyoo: {words[Math.floor(Math.random() * words.length)]}
-      </GameWrapper>
-    );
-  }
+const StyledInput = styled.input`
+  opacity: 0;
+`
+
+const Input = () => {
+  let textInput = null;
+  useEffect(() => {
+    textInput.focus();
+  });
+
+  return(
+    <StyledInput
+      type="text"
+      id="hidden-input"
+      aria-label="Type test input"
+      ref={(input) => {
+        textInput = input;
+      }}
+      onBlur={() => {
+        setInterval(() => {
+          textInput.focus();}, 50);
+      }}
+      onChange={() => {
+        console.log(textInput.value); // gör nått vettigt här
+      }}
+    />
+  );
 }
+
+// Retrieves a random word from imported array of words, return array of letters
+const getWord = (words) => {
+  return words[Math.floor(Math.random() * words.length)].split('');
+}
+
+// Load a word
+let wordArr = getWord(words);
+
+const TypeTest = () => {
+
+  return(
+    <TypeTestWrapper>
+      <Text>
+        {
+          wordArr.map((letter, i) => {
+            return <span key={i}>{letter}</span>
+          })
+        }
+      </Text>
+      <Input />
+    </TypeTestWrapper>
+  );
+}
+
+export default TypeTest;
